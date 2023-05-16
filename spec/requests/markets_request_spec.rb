7cfbd4f98f0ad2ffb938104d1b0e97f37a5b4fc6
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-16 14:04:23 -0600

    test: Add happy path tests for getting one market.

--- spec/requests/markets_request_spec.rb
+++ spec/requests/markets_request_spec.rb
@@ -46,4 +46,49 @@ describe 'Market API' do
       expect(market_atrb[:vendor_count]).to be_an(Integer)
     end
   end
+
+  it 'can get one market by its id' do
+    id = create(:market).id
+
+    get "/api/v0/markets/#{id}"
+
+    expect(response).to be_successful
+
+    market = JSON.parse(response.body, symbolize_names: true)
+
+    market_data = market[:data]
+
+    expect(market_data).to have_key(:id) # Ask about this checkin, is this redundnant
+    expect(market_data[:id]).to be_an(String)
+
+    market_atrb = market_data[:attributes]
+
+    expect(market_atrb).to have_key(:name)
+    expect(market_atrb[:name]).to be_a(String)
+
+    expect(market_atrb).to have_key(:street)
+    expect(market_atrb[:street]).to be_a(String)
+
+    expect(market_atrb).to have_key(:city)
+    expect(market_atrb[:city]).to be_a(String)
+
+    expect(market_atrb).to have_key(:county)
+    expect(market_atrb[:county]).to be_a(String)
+
+    expect(market_atrb).to have_key(:state)
+    expect(market_atrb[:state]).to be_an(String)
+
+    expect(market_atrb).to have_key(:zip)
+    expect(market_atrb[:zip]).to be_an(String)
+
+    expect(market_atrb).to have_key(:lat)
+    expect(market_atrb[:lat]).to be_an(String)
+
+    expect(market_atrb).to have_key(:lon)
+    expect(market_atrb[:lon]).to be_an(String)
+
+    expect(market_atrb).to have_key(:vendor_count)
+    expect(market_atrb[:vendor_count]).to be_an(Integer)
+  end
+ 
 end
