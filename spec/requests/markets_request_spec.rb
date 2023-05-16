58434e3c8896a9041400808180857e25de14a9dd
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-16 12:47:57 -0600

    fix: fix variables to account for serialization.

--- spec/requests/markets_request_spec.rb
+++ spec/requests/markets_request_spec.rb
@@ -10,35 +10,38 @@ describe 'Market API' do
 
     markets = JSON.parse(response.body, symbolize_names: true)
 
-    expect(markets.count).to eq(3)
+    expect(markets[:data].count).to eq(3)
 
-    markets.each do |market|
+    markets[:data].each do |market|
       expect(market).to have_key(:id) # Ask about this checkin, is this redundnant
-      expect(market[:id]).to be_an(Integer)
+      expect(market[:id]).to be_an(String)
 
-      expect(market).to have_key(:name)
-      expect(market[:name]).to be_a(String)
+      expect(market[:attributes]).to have_key(:name)
+      expect(market[:attributes][:name]).to be_a(String)
 
-      expect(market).to have_key(:street)
-      expect(market[:street]).to be_a(String)
+      expect(market[:attributes]).to have_key(:street)
+      expect(market[:attributes][:street]).to be_a(String)
 
-      expect(market).to have_key(:city)
-      expect(market[:city]).to be_a(String)
+      expect(market[:attributes]).to have_key(:city)
+      expect(market[:attributes][:city]).to be_a(String)
 
-      expect(market).to have_key(:county)
-      expect(market[:county]).to be_a(String)
+      expect(market[:attributes]).to have_key(:county)
+      expect(market[:attributes][:county]).to be_a(String)
 
-      expect(market).to have_key(:state)
-      expect(market[:state]).to be_an(String)
+      expect(market[:attributes]).to have_key(:state)
+      expect(market[:attributes][:state]).to be_an(String)
 
-      expect(market).to have_key(:zip)
-      expect(market[:zip]).to be_an(String)
+      expect(market[:attributes]).to have_key(:zip)
+      expect(market[:attributes][:zip]).to be_an(String)
 
-      expect(market).to have_key(:lat)
-      expect(market[:lat]).to be_an(String)
+      expect(market[:attributes]).to have_key(:lat)
+      expect(market[:attributes][:lat]).to be_an(String)
 
-      expect(market).to have_key(:lon)
-      expect(market[:lon]).to be_an(String)
+      expect(market[:attributes]).to have_key(:lon)
+      expect(market[:attributes][:lon]).to be_an(String)
+
+      expect(market[:attributes]).to have_key(:vendor_count)
+      expect(market[:attributes][:vendor_count]).to be_an(Integer)
     end
   end
 end
