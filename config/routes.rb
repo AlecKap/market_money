0d03d752d18abb11348b88d7c47373baca682a1d
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-16 14:03:52 -0600

    route: Add show to market resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
-      resources :markets, only: [:index]
+      resources :markets, only: [:index, :show]
     end
   end
 end
