5057dca152109675c992db3b942220c8606b6cc2
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-16 17:34:41 -0600

    feat: Add show action.

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -3,4 +3,8 @@ class Api::V0::VendorsController < ApplicationController
     market = Market.find(params[:market_id])
     render json: VendorSerializer.new(market.vendors)
   end
+
+  def show
+    render json: VendorSerializer.new(Vendor.find(params[:id]))
+  end
 end
