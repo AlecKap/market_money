85998ff5f46d90b773dc9004592837e80c8da2b9
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-16 17:11:55 -0600

    refactor: Add test data for rails helper.

--- spec/models/market_spec.rb
+++ spec/models/market_spec.rb
@@ -7,16 +7,9 @@ RSpec.describe Market, type: :model do
   describe 'instance methods' do
     describe 'vendor_count' do
       it 'returns a markets count of vendors' do
-        market1 = create(:market)
-        vendor1 = create(:vendor)
-        vendor2 = create(:vendor)
-        vendor3 = create(:vendor)
-        vendor4 = create(:vendor)
-        create(:market_vendor, market: market1, vendor: vendor1)
-        create(:market_vendor, market: market1, vendor: vendor2)
-        create(:market_vendor, market: market1, vendor: vendor3)
+        test_data
 
-        expect(market1.vendor_count).to eq(3)
+        expect(@market1.vendor_count).to eq(3)
       end
     end
   end
