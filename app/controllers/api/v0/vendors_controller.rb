d9df71f402faca35a26e3ffa3a09e3fbbf025c64
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-16 19:52:01 -0600

    feat: Add create action to vendors controller.

--- app/controllers/api/v0/vendors_controller.rb
+++ app/controllers/api/v0/vendors_controller.rb
@@ -7,4 +7,18 @@ class Api::V0::VendorsController < ApplicationController
   def show
     render json: VendorSerializer.new(Vendor.find(params[:id]))
   end
+
+  def create
+    render json: VendorSerializer.new(Vendor.create!(vendor_params))
+  end
+
+  private
+
+  def vendor_params
+    params.require(:vendor).permit(:name,
+                                   :description,
+                                   :contact_name,
+                                   :contact_phone,
+                                   :credit_accepted)
+  end
 end
