8895f5049a82e0d2d55273b327351b3e50274c0f
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-16 19:49:55 -0600

    test: Add happy path test for creating a new vendor.

--- spec/requests/vendors_request_spec.rb
+++ spec/requests/vendors_request_spec.rb
@@ -52,4 +52,28 @@ describe 'Vendor API' do
       expect(error_detail[0][:details]).to eq("Couldn't find Vendor with 'id'=#{id}")
     end
   end
+
+  describe 'can create a new vendor' do
+    it 'with valid information' do
+      vendor_params = ({
+        name: 'Murder on the Orient Express',
+        description: 'Agatha Christie',
+        contact_name: 'mystery',
+        contact_phone: 'Filled with suspense.',
+        credit_accepted: true
+        })
+      headers = { 'CONTENT_TYPE' => 'application/json' }
+
+      post '/api/v0/vendors', headers: headers, params: JSON.generate(vendor: vendor_params)
+
+      created_vendor = Vendor.last
+
+      expect(response).to be_successful
+      expect(created_vendor.name).to eq(vendor_params[:name])
+      expect(created_vendor.description).to eq(vendor_params[:description])
+      expect(created_vendor.contact_phone).to eq(vendor_params[:contact_phone])
+      expect(created_vendor.contact_name).to eq(vendor_params[:contact_name])
+      expect(created_vendor.credit_accepted).to eq(vendor_params[:credit_accepted])
+    end
+  end
 end
