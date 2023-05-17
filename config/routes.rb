7a08e0bc0b96caca0f4ba294ae012c60c58fb9ad
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-16 19:51:12 -0600

    route: Add creat action to vendors resources.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
         resources :vendors, only: [:index]
       end
 
-      resources :vendors, only: [:show]
+      resources :vendors, only: [:show, :create]
     end
   end
 end
