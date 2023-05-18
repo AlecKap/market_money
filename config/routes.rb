5623793cb553d1134226e0f08c81702bc76537b0
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-17 23:38:45 -0600

    route: Add destroy resource.

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v0 do
       resources :market_vendors, only: [:create]
+      resource :market_vendors, only: [:destroy]
       resources :vendors, only: [:show, :create, :update, :destroy]
       resources :markets, only: [:index, :show] do
         resources :vendors, only: [:index], controller: 'markets/vendors'
