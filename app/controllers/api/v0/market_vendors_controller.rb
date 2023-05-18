ae056deb7def6f9e6d574271034d09f6451e2459
AlecKap <120225785+AlecKap@users.noreply.github.com>
2023-05-17 23:39:30 -0600

    feat: Add destroy action.

--- app/controllers/api/v0/market_vendors_controller.rb
+++ app/controllers/api/v0/market_vendors_controller.rb
@@ -1,7 +1,18 @@
 class Api::V0::MarketVendorsController < ApplicationController
+  before_action :params_ids, only: [:destroy]
   def create
     MarketVendor.create!(market_vendor_params)
     render_success_response
+  end
+
+  def destroy
+    begin
+      MarketVendor.find_by!(market_vendor_params).destroy
+    rescue ActiveRecord::RecordNotFound
+      render json: ErrorSerializer.serializer(
+        "No MarketVendor with market_id=#{@market_id} AND vendor_id=#{@vendor_id} exists"),
+        status: :not_found
+    end
   end
 
   private
@@ -12,5 +23,10 @@ class Api::V0::MarketVendorsController < ApplicationController
 
   def render_success_response
     render json: { message: 'Successfully added vendor to market' }, status: 201
+  end
+
+  def params_ids
+    @market_id = params[:market_vendor][:market_id]
+    @vendor_id = params[:market_vendor][:vendor_id]
   end
 end
